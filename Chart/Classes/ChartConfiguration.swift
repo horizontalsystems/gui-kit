3a8459b7c333ba22fe13bdcda8f123bf8c1b4037
Anton Stavnichiy <md.belichenko@gmail.com>
2019-12-18 12:25:44 +0600

    Make public parameters in configuration

--- Chart/Classes/ChartConfiguration.swift
+++ Chart/Classes/ChartConfiguration.swift
@@ -1,52 +1,52 @@
 import UIKit
 
 public class ChartConfiguration {
-    var showGrid: Bool = true
-    var showLimitValues: Bool = true
+    public var showGrid: Bool = true
+    public var showLimitValues: Bool = true
 
-    var animationDuration: TimeInterval = 0.3
+    public var animationDuration: TimeInterval = 0.3
 
-    var backgroundColor: UIColor = .clear
+    public var backgroundColor: UIColor = .clear
 
-    var chartInsets: UIEdgeInsets = .zero
-    var curveWidth: CGFloat = 1
-    var curvePositiveColor: UIColor = .green
-    var curveNegativeColor: UIColor = .red
-    var curveIncompleteColor: UIColor = .gray
+    public var chartInsets: UIEdgeInsets = .zero
+    public var curveWidth: CGFloat = 1
+    public var curvePositiveColor: UIColor = .green
+    public var curveNegativeColor: UIColor = .red
+    public var curveIncompleteColor: UIColor = .gray
 
-    var curvePercentPadding: CGFloat = 0.1
+    public var curvePercentPadding: CGFloat = 0.1
 
-    var gradientPositiveColor: UIColor = .green
-    var gradientNegativeColor: UIColor = .red
-    var gradientIncompleteColor: UIColor = .gray
-    var gradientStartTransparency: CGFloat = 0.8
-    var gradientFinishTransparency: CGFloat = 0.05
+    public var gradientPositiveColor: UIColor = .green
+    public var gradientNegativeColor: UIColor = .red
+    public var gradientIncompleteColor: UIColor = .gray
+    public var gradientStartTransparency: CGFloat = 0.8
+    public var gradientFinishTransparency: CGFloat = 0.05
 
-    var gridNonVisibleLineDeltaX: CGFloat = 5                           // if timestamp line drawing near sides lines we must draw only sides line
-    var valueDigitDiff: Int = 5
-    var gridMaxScale: Int = 8
+    public var gridNonVisibleLineDeltaX: CGFloat = 5                           // if timestamp line drawing near sides lines we must draw only sides line
+    public var valueDigitDiff: Int = 5
+    public var gridMaxScale: Int = 8
 
-    var limitColor: UIColor = .white
+    public var limitColor: UIColor = .white
 
-    var gridColor: UIColor = .white
-    var gridTextColor: UIColor = .white
-    var gridTextFont: UIFont = .systemFont(ofSize: 12)
+    public var gridColor: UIColor = .white
+    public var gridTextColor: UIColor = .white
+    public var gridTextFont: UIFont = .systemFont(ofSize: 12)
 
-    var gridTextMargin: CGFloat = 4
+    public var gridTextMargin: CGFloat = 4
 
-    var limitTextFont: UIFont = .systemFont(ofSize: 12)
-    var limitTextColor: UIColor = .white
-    var limitTextOutMargin: CGFloat = 8
-    var limitTextInMargin: CGFloat = 4
-    var limitTextLeftMargin: CGFloat = 16
+    public var limitTextFont: UIFont = .systemFont(ofSize: 12)
+    public var limitTextColor: UIColor = .white
+    public var limitTextOutMargin: CGFloat = 8
+    public var limitTextInMargin: CGFloat = 4
+    public var limitTextLeftMargin: CGFloat = 16
 
-    var limitTextFormatter: NumberFormatter?
-    var dateFormatter: DateFormatter?
+    public var limitTextFormatter: NumberFormatter?
+    public var dateFormatter: DateFormatter?
 
-    var selectedCircleRadius: CGFloat = 5.5
-    var selectedIndicatorColor: UIColor = .white
-    var selectedCurveColor: UIColor = .white
-    var selectedGradientColor: UIColor = .white
+    public var selectedCircleRadius: CGFloat = 5.5
+    public var selectedIndicatorColor: UIColor = .white
+    public var selectedCurveColor: UIColor = .white
+    public var selectedGradientColor: UIColor = .white
 
     public init() {}
 
