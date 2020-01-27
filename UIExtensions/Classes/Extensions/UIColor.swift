3e108c88253bd202a3a3cd57ec4e434955457e6a
Ermat <ealymbaev@gmail.com>
2020-01-27 11:37:26 +0600

    UIExtensions: add new extensions
    - UIButton, UIFont, UIImage, UIWindow

--- UIExtensions/Classes/Extensions/UIColor.swift
+++ UIExtensions/Classes/Extensions/UIColor.swift
@@ -2,18 +2,14 @@ import UIKit
 
 extension UIColor {
 
-    // Usage: UIColor(hex: 0xFC0ACE)
-    public convenience init(hex: Int) {
-        self.init(hex: hex, alpha: 1)
-    }
-
-    // Usage: UIColor(hex: 0xFC0ACE, alpha: 0.25)
+    // Usage: UIColor(hex: 0xfc0ace, alpha: 0.25)
     public convenience init(hex: Int, alpha: Double = 1) {
         self.init(
                 red: CGFloat((hex >> 16) & 0xff) / 255,
                 green: CGFloat((hex >> 8) & 0xff) / 255,
                 blue: CGFloat(hex & 0xff) / 255,
-                alpha: CGFloat(alpha))
+                alpha: CGFloat(alpha)
+        )
     }
 
 }
