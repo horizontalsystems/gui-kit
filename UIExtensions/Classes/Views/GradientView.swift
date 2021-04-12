eb8097d8be969f5b2835bba80f7ed9a98c591e36
max <trickster77777@gmail.com>
2021-04-12 15:00:06 +0600

    Make bottom gradient listen system theme

--- UIExtensions/Classes/Views/GradientView.swift
+++ UIExtensions/Classes/Views/GradientView.swift
@@ -3,15 +3,22 @@ import UIKit
 open class GradientView: UIView {
     private let gradientLayer = CAGradientLayer()
 
+    private let fromColor: UIColor
+    private let toColor: UIColor
     private let gradientHeight: CGFloat
 
     public init(gradientHeight: CGFloat, fromColor: UIColor, toColor: UIColor) {
         self.gradientHeight = gradientHeight
+        self.fromColor = fromColor
+        self.toColor = toColor
+
         super.init(frame: .zero)
-        gradientLayer.colors = [fromColor.cgColor, toColor.cgColor]
+
         gradientLayer.locations = [0, 1]
         gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
         layer.addSublayer(gradientLayer)
+
+        updateUI()
     }
 
     required public init?(coder aDecoder: NSCoder) {
@@ -23,6 +30,16 @@ open class GradientView: UIView {
         gradientLayer.frame = bounds
         let endPosition: CGFloat = gradientHeight / bounds.height
         gradientLayer.endPoint = CGPoint(x: 0.5, y: endPosition)
+    }
+
+    public override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+
+        updateUI()
+    }
+
+    private func updateUI() {
+        gradientLayer.colors = [fromColor.cgColor, toColor.cgColor]
     }
 
 }
