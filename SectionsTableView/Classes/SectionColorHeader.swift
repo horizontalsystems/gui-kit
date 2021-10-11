83bc9ebacfed756a7706e6d0d0ec5dbb01505e6d
Ermat <ealymbaev@gmail.com>
2021-10-11 17:02:17 +0600

    SectionTableView: disable interaction with section color header/footer

--- SectionsTableView/Classes/SectionColorHeader.swift
+++ SectionsTableView/Classes/SectionColorHeader.swift
@@ -5,11 +5,13 @@ public class SectionColorHeader: UITableViewHeaderFooterView {
     public override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
         backgroundView = UIView()
+        isUserInteractionEnabled = false
     }
 
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         backgroundView = UIView()
+        isUserInteractionEnabled = false
     }
 
 }
