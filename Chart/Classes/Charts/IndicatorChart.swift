84ebaa5b76cb5522eeb17f109e16f9b25c18cb2b
ant013 <ant013@mail.ru>
2022-05-18 15:03:25 +0600

    Update chart UI

--- Chart/Classes/Charts/IndicatorChart.swift
+++ Chart/Classes/Charts/IndicatorChart.swift
@@ -2,11 +2,15 @@ import UIKit
 
 class IndicatorChart: Chart {
     private let volumeBars = ChartBars()
+    private let verticalLines = ChartGridLines()
+    private let horizontalLines = ChartGridLines()
 
     init(configuration: ChartConfiguration? = nil) {
         super.init(frame: .zero)
 
+        add(verticalLines)
         add(volumeBars)
+        add(horizontalLines)
 
         if let configuration = configuration {
             apply(configuration: configuration)
@@ -23,6 +27,20 @@ class IndicatorChart: Chart {
         volumeBars.padding = configuration.volumeBarsInsets
         volumeBars.animationDuration = configuration.animationDuration
 
+        if configuration.showVerticalLines {
+            verticalLines.gridType = .vertical
+            verticalLines.lineDirection = .top
+            verticalLines.lineWidth = configuration.verticalLinesWidth
+            verticalLines.invisibleIndent = configuration.verticalInvisibleIndent
+
+            verticalLines.strokeColor = configuration.verticalLinesColor
+        }
+
+        horizontalLines.gridType = .horizontal
+        horizontalLines.lineWidth = configuration.borderWidth
+        horizontalLines.strokeColor = configuration.borderColor
+        horizontalLines.set(points: [CGPoint(x: 0, y: 0), CGPoint(x: 1, y: 0)])
+
         return self
     }
 
@@ -37,6 +55,14 @@ class IndicatorChart: Chart {
 
     func setVolumes(hidden: Bool) {
         volumeBars.layer.isHidden = hidden
+    }
+
+    func setVerticalLines(points: [CGPoint], animated: Bool = false) {
+        verticalLines.set(points: points, animated: animated)
+    }
+
+    func setVerticalLines(hidden: Bool) {
+        verticalLines.layer.isHidden = hidden
     }
 
 }
